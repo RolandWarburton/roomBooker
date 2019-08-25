5bccc0644c0bfdcb2b813e34b4c53cdc855796e5
BuildTools <warburtonroland@gmail.com>
2019-08-25 20:15:14 +1000

    can look through rooms and days

--- src/automate.py
+++ src/automate.py
@@ -24,6 +24,7 @@ def waitUntilIDLoaded(element):
 	finally:
     		browser.quit
 
+
 def waitUntilXpathElementLoaded(path):
 	try:
     		WebDriverWait(browser, 10).until(
@@ -43,28 +44,52 @@ def checkPageTitle(title):
     		browser.quit
 	print(title + " passed validation ")
 
-def getDailyBookings():
+
+def getRooms():
+	waitUntilIDLoaded("bookingGridContent")
 	room1 = browser.find_elements_by_css_selector("#bookingStrip1434 .ts")
+	room2 = browser.find_elements_by_css_selector("#bookingStrip1435 .ts")
+	room3 = browser.find_elements_by_css_selector("#bookingStrip1436 .ts")
+	room4 = browser.find_elements_by_css_selector("#bookingStrip1437 .ts")
+	room5 = browser.find_elements_by_css_selector("#bookingStrip1438 .ts")
+	rooms = [room1, room2, room3, room4, room5]
+	return rooms
+
+def getBookings():
 	times = []
 	timeslots = []
-	for i, room in enumerate(room1):
-		col = room.get_attribute('alt').split(" ")
-	if len(col) == 3:
-		timeparts = col[0].split(":")
-		print(timeparts)
-		if int(timeparts[0]) > firstSuitableBookTime and int(timeparts[0]) < lastSuitableBookTime:
-			times.append(col[0])
-	else:
-		if len(times) != 0:
-			timeslots.append(times)
-		times = []
-	return timeslots 
+
+	for i in range(6):
+		print("DAY: " + str(i+1))
+		rooms = getRooms()
+		for i, room in enumerate(rooms):
+			print("room: " + str(i))
+			for timeslot in room:
+				col = timeslot.get_attribute('alt').split(" ")
+				if len(col) == 3:
+					timeparts = col[0].split(":")
+					if int(timeparts[0]) > firstSuitableBookTime and int(timeparts[0]) < lastSuitableBookTime:
+						times.append(col[0])
+				else:
+					if len(times) >= 8:
+						timeslots.append(times)
+						return timeslots
+					times = []
+		if len(timeslots) == 0:
+			waitUntilXpathElementLoaded("//a[@title = 'Next']")
+			nextButton = browser.find_element_by_xpath("//a[@title = 'Next']")
+			nextButton.click()
+		else:
+			break	
+	return timeslots
+
 
 print("start: ")
 f = open('loginInfo.json', 'r')
 info = json.load(f)
 login = info['login']
 secret = info['secret']
+f.close()
 
 browser = webdriver.Chrome()
 browser.get("https://pcbooking.swin.edu.au/cire/login.aspx?ViewSimpleMode=false")
@@ -85,44 +110,18 @@ checkPageTitle("Room and PC Booking")
 waitUntilElementLoaded("locationTable")
 
 
-# f = open("output.txt", "w")
-# print(browser.find_element_by_tag_name("title"))
 print("attempting to extract booking data")
-
-# query = "disableGrid();retrieveHtmlFromUrl(\'bookingGrid\',\'Front.aspx?page=bookingGrid&date=26/08/2019\', \'adjustBookingGridView();\'); return false;"
-
-# room2 = browser.find_elements_by_css_selector("#bookingStrip1435 .ts")
-# room3 = browser.find_elements_by_css_selector("#bookingStrip1435 .ts")
-# room4 = browser.find_elements_by_css_selector("#bookingStrip1435 .ts")
-
-# go to the next day
-
-
 
 
 
 firstSuitableBookTime = 9
-lastSuitableBookTime = 24
+lastSuitableBookTime = 17
 # def satisfyTime(time1, time2, length):
 # 	print(time1 + "\n")
 # 	print(time2 + "\n")
 # 	return False
+timeslots = getBookings()
 
 
-for i in range(6):
-	print("test1")
-	waitUntilIDLoaded("bookingGridContent")
-	print("test2")
-	timeslots = getDailyBookings()
-	print("test3")
-	waitUntilXpathElementLoaded("//a[@title = 'Next']")
-	print("test4")
-	nextButton = browser.find_element_by_xpath("//a[@title = 'Next']")
-	print("test5")
-	nextButton.click()
-	
 
-
-# for time in timeslots:
-# 	if time[0] > 
-print(timeslots)
+print("TIMESLOTS: " + str(timeslots))
